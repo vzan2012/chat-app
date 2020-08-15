a676476d8e7dcece83c04f57432b60d0295c92ff
Deepak Guptha <vzan2012@gmail.com>
2020-08-15 18:35:54 +0530

    feat: Configuring the Web Sockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const path = require("path");
-
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
+
 const app = new express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 
@@ -9,4 +13,6 @@ const publicDirectoyPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoyPath));
 
-app.listen(port, () => console.log(`App running on PORT ${port}`));
+io.on("connection", () => console.log("New WebSocket Connection"));
+
+server.listen(port, () => console.log(`App running on PORT ${port}`));
