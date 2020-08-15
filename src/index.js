4c2a1bf467f487d4f7e6d3872932dde08fe29e96
Deepak Guptha <vzan2012@gmail.com>
2020-08-15 14:19:25 +0530

    feat: Configure public folder to serve

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 const app = new express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+const publicDirectoyPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoyPath));
 
 app.listen(port, () => console.log(`App running on PORT ${port}`));
