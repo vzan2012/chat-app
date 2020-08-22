835a5df578b293769862596d2f136ae8bbf6aafa
Deepak Guptha <vzan2012@gmail.com>
2020-08-22 17:44:13 +0530

    feat: event acknowledgement for messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,11 @@ socket.on("message", (result) => console.log(result));
 document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
   // socket.emit("sendMessage", document.querySelector("#txtMessage").value);
-  socket.emit("sendMessage", e.target.elements.message.value);
+  socket.emit("sendMessage", e.target.elements.message.value, (error) => {
+    // console.log("Message has been delivered ", message)
+    if (error) return console.log(error);
+    console.log("Message Delivered !!!");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
@@ -16,6 +20,8 @@ document.querySelector("#send-location").addEventListener("click", () => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    socket.emit("sendLocation", { latitude, longitude });
+    socket.emit("sendLocation", { latitude, longitude }, () => {
+      console.log("Location Shared");
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = new express();
 const server = http.createServer(app);
@@ -17,14 +18,20 @@ io.on("connection", (socket) => {
   console.log("New WebSocket Connection");
 
   socket.broadcast.emit("message", "New User has joined");
-  socket.on("sendMessage", (data) => io.emit("message", data));
+  socket.on("sendMessage", (data, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(data)) return callback("Profanity is not allowed");
+    io.emit("message", data);
+    callback("Delivered !!!");
+  });
 
-  socket.on("sendLocation", (position) =>
+  socket.on("sendLocation", (position, callback) => {
     io.emit(
       "message",
       `https://www.google.com/maps?q=${position.latitude},${position.longitude}`
-    )
-  );
+    );
+    callback();
+  });
 
   socket.on("disconnect", () => io.emit("message", "A User has left"));
 });
