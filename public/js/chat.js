23c58c57b353648c265ac5ee9c81d88e838b76b5
Deepak Guptha <vzan2012@gmail.com>
2020-08-30 19:29:57 +0530

    feat: Updation od user list in sidebar

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,7 @@ const $messages = document.querySelector("#messages");
 const $messageTemplate = document.querySelector("#message-template").innerHTML;
 const $locationTemplate = document.querySelector("#location-message-template")
   .innerHTML;
+const $sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, {
@@ -35,6 +36,15 @@ socket.on("locationMessage", (message) => {
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render($sidebarTemplate, {
+    room,
+    users,
+  });
+
+  document.querySelector("#sidebar").innerHTML = html;
 });
 
 // $messageFormButton.setAttribute("disabled", "disabled");
--- src/index.js
+++ src/index.js
@@ -44,6 +44,11 @@ io.on("connection", (socket) => {
       .to(user.room)
       .emit("message", generateMessage("Admin", `${user.username} has joined`));
 
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
     callback();
   });
 
@@ -73,11 +78,17 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
-    if (user)
+    if (user) {
       io.to(user.room).emit(
         "message",
         generateMessage("Admin", `${user.username} has left`)
       );
+
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
   });
 });
 
